7c305fd8ea71c36a73046023abf5201efdf468a4
Nelson Letelier <n.letelier.v@gmail.com>
2012-04-09 21:00:56 -0400

    ADD new blanck application

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Cheetahquotes::Application.config.secret_token = 'cd116d7b478664afbec230166944c0a3d43f0e6fb0ed49776591465e0bc258014428f1124c651b3bb947b383d0c86dbe15d462c60949f74ad74d4ce936a8efd4'
